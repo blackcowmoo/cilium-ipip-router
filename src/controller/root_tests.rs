76a0fcf02562b9c92279ca053bd2288c7580925c
blackcowmooo <blackcowmooo@gmail.com>
2026-04-01 16:51:33 +0000

    refactor: simplify test imports by removing ControllerHandle usage
    - Remove unused ControllerHandle import from root_tests.rs
    - Replace ControllerHandle::new() calls with direct usage of builder.cmd_tx
    - Clean up test code by removing unnecessary controller initialization
    
    Author: blackcowmooo <blackcowmooo@gmail.com>

--- src/controller/root_tests.rs
+++ src/controller/root_tests.rs
@@ -1,19 +1,19 @@
-use crate::controller::{builder::ControllerBuilder, handle::ControllerHandle};
+use crate::controller::builder::ControllerBuilder;
 
 #[tokio::test]
 async fn test_watch_node_added() {
     let builder = ControllerBuilder::new();
-    let _controller = ControllerHandle::new(builder.cmd_tx);
+    let _ = builder.cmd_tx;
 }
 
 #[tokio::test]
 async fn test_watch_node_modified() {
     let builder = ControllerBuilder::new();
-    let _controller = ControllerHandle::new(builder.cmd_tx);
+    let _ = builder.cmd_tx;
 }
 
 #[tokio::test]
 async fn test_watch_node_deleted() {
     let builder = ControllerBuilder::new();
-    let _controller = ControllerHandle::new(builder.cmd_tx);
+    let _ = builder.cmd_tx;
 }
