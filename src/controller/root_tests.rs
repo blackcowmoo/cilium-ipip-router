cfed4bed6a77b433150adc7b99a995829a1215a7
blackcowmooo <blackcowmooo@gmail.com>
2026-04-02 01:41:16 +0000

    refactor: simplify root_tests by removing unused mock and tests

--- src/controller/root_tests.rs
+++ src/controller/root_tests.rs
@@ -1,40 +1,8 @@
 use crate::controller::builder::ControllerBuilder;
-use crate::controller::root::ControllerInner;
 use k8s_openapi::api::core::v1::Node;
-use kube::api::{Api, ListParams, WatchEvent, WatchParams};
-use mockall::mock;
-
-mock! {
-    pub NodeApi {}
-
-    impl<'a> Api<Node> for NodeApi {
-        fn name(&self) -> &str;
-        fn namespace(&self) -> &str;
-        fn list(&self, lp: &ListParams) -> kube::Result<kube::api::ObjectList<Node>>;
-        fn get(&self, name: &str) -> kube::Result<Node>;
-        fn delete(&self, name: &str, params: &ListParams) -> kube::Result<()>;
-        fn watch(
-            &self,
-            params: &WatchParams,
-            since: &str,
-        ) -> kube::Result<kube::watch::Watcher<std::pin::Pin<Box<dyn std::io::Read + Send>>>>;
-    }
-}
 
 #[tokio::test]
-async fn test_watch_node_added() {
-    let builder = ControllerBuilder::new();
-    let _ = builder.cmd_tx;
-}
-
-#[tokio::test]
-async fn test_watch_node_modified() {
-    let builder = ControllerBuilder::new();
-    let _ = builder.cmd_tx;
-}
-
-#[tokio::test]
-async fn test_watch_node_deleted() {
+async fn test_controller_builder() {
     let builder = ControllerBuilder::new();
     let _ = builder.cmd_tx;
 }
