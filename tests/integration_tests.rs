c19862e189f4911f52213f6638c1b3dab9e6647b
blackcowmooo <blackcowmooo@gmail.com>
2026-05-02 16:56:31 +0000

    refactor: remove trailing blank lines from integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -6,15 +6,15 @@ async fn test_controller_connectivity() -> anyhow::Result<()> {
     let client = Client::try_default()
         .await
         .inspect_err(|e| log::error!("Failed to create Kubernetes client: {}", e))?;
-    
+
     let nodes: Api<Node> = Api::all(client.clone());
-    
+
     let node_list = nodes.list(&Default::default()).await.inspect_err(|e| {
         log::error!("Failed to list nodes: {}", e);
     })?;
-    
+
     log::info!("Found {} nodes in cluster", node_list.items.len());
-    
+
     Ok(())
 }
 
@@ -23,14 +23,14 @@ async fn test_node_watch_connection() -> anyhow::Result<()> {
     let client = Client::try_default()
         .await
         .inspect_err(|e| log::error!("Failed to create Kubernetes client: {}", e))?;
-    
+
     let nodes: Api<Node> = Api::all(client.clone());
-    
+
     let lp = kube::api::ListParams::default();
-    
+
     let _stream = nodes.watch(&lp, "0").await.inspect_err(|e| {
         log::error!("Failed to setup watch stream: {}", e);
     })?;
-    
+
     Ok(())
 }
